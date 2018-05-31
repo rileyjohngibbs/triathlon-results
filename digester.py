1a601d04fb7201122c0b23ff36aa7d953f48e221
Riley Gibbs <riley@citizennet.com>
2018-05-30 19:06:04 -0700

    Added command line options
    Can now specify the fields to look for in the CSV for segments.
    Can now specify the field to look for in the VSC for the total time.

--- digester.py
+++ digester.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import OrderedDict
 from csv import DictReader
 from functools import reduce
@@ -7,14 +8,18 @@ from typing import Dict, List, Mapping, Union
 import unittest
 
 SAMPLE_RESULTS_PATH = "sample_results.csv"
-SEGMENTS = ["Swim", "T1", "Bike", "T2", "Run"]
+DEFAULT_SEGMENTS = ["Swim", "T1", "Bike", "T2", "Run"]
 TOTAL_KEY = "Gun"
 
 
-def load_and_format_results(path: str=SAMPLE_RESULTS_PATH) -> List[Dict[str, Union[int, str]]]:
+def load_and_format_results(
+            path: str,
+            segments: List[str],
+            total_key: str
+        ) -> List[Dict[str, Union[int, str]]]:
     unformatted_results = load_results(path)
-    parsed_results = [get_segment_times(result) for result in unformatted_results]
-    patched_results = estimate_missing_results_values(parsed_results)
+    parsed_results = [get_segment_times(result, segments, total_key) for result in unformatted_results]
+    patched_results = estimate_missing_results_values(parsed_results, segments, total_key)
     return patched_results
 
 
@@ -25,11 +30,15 @@ def load_results(path: str=SAMPLE_RESULTS_PATH) -> List[Dict[str, str]]:
     return rows
 
 
-def get_segment_times(row: Dict[str, str]) -> Dict[str, Union[int, str]]:
+def get_segment_times(
+            row: Dict[str, str],
+            segments: List[str],
+            total_key: str
+        ) -> Dict[str, Union[int, str]]:
     new_row = {}
     for key, value in row.items():
         new_value: Union[int, str]
-        if key in SEGMENTS or key == TOTAL_KEY:
+        if key in segments or key == total_key:
             new_value = parse_time(value)
         else:
             new_value = value
@@ -45,17 +54,27 @@ def parse_time(time_string: str) -> int:
     return reduce(lambda x, y: 60*x + y, tokens)
 
 
-def estimate_missing_results_values(results: List[Dict[str, Union[int, str]]]) \
-        -> List[Dict[str, Union[int, str]]]:
-    segment_proportions = calculate_proportions(results)
-    return [estimate_missing_row_values(row, segment_proportions) for row in results]
+def estimate_missing_results_values(
+            results: List[Dict[str, Union[int, str]]],
+            segments: List[str],
+            total_key: str
+        ) -> List[Dict[str, Union[int, str]]]:
+    segment_proportions = calculate_proportions(results, segments, total_key)
+    return [
+        estimate_missing_row_values(row, segment_proportions, segments, total_key)
+        for row in results
+    ]
 
 
-def calculate_proportions(results: List[Dict[str, Union[int, str]]]) -> Dict[str, float]:
-    total_time = sum(int(row[TOTAL_KEY]) for row in results)
+def calculate_proportions(
+            results: List[Dict[str, Union[int, str]]],
+            segments: List[str],
+            total_key: str
+        ) -> Dict[str, float]:
+    total_time = sum(int(row[total_key]) for row in results)
     segment_proportions = {
         segment: sum(int(row[segment]) for row in results) / total_time
-        for segment in SEGMENTS
+        for segment in segments
     }
     return segment_proportions
 
@@ -63,11 +82,13 @@ def calculate_proportions(results: List[Dict[str, Union[int, str]]]) -> Dict[str
 
 def estimate_missing_row_values(
             row: Dict[str, Union[int, str]],
-            proportions: Dict[str, float]
+            proportions: Dict[str, float],
+            segments: List[str],
+            total_key: str
         ) -> Dict[str, Union[int, str]]:
-    missing_segments = [segment for segment in SEGMENTS if row[segment] == 0]
-    counted_time = sum(int(row[segment]) for segment in SEGMENTS)
-    uncounted_time = int(row[TOTAL_KEY]) - counted_time
+    missing_segments = [segment for segment in segments if row[segment] == 0]
+    counted_time = sum(int(row[segment]) for segment in segments)
+    uncounted_time = int(row[total_key]) - counted_time
     total_proportion_uncounted = sum(proportions[segment] for segment in missing_segments)
     new_row = {}
     for key, value in row.items():
@@ -93,14 +114,24 @@ class Tests(unittest.TestCase):
         proportions = {"Swim": 0.2, "T1": 0.03, "Bike": 0.45, "T2": 0.02, "Run": 0.3}
         expected_swim = 522
         expected_t1 = 78
-        estimates = estimate_missing_row_values(row, proportions)
+        estimates = estimate_missing_row_values(row, proportions, DEFAULT_SEGMENTS, TOTAL_KEY)
         self.assertEqual(estimates["Swim"], expected_swim)
         self.assertEqual(estimates["T1"], expected_t1)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        filepath = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", help="location of the csv with the results")
+    parser.add_argument("--segments", help="comma-separated string of segment headers")
+    parser.add_argument("--totalheader", help="header for the total time")
+    args = parser.parse_args()
+    filepath = args.filepath
+    if args.segments:
+        segments = [s.strip() for s in args.segments.split(",")]
     else:
-        filepath = SAMPLE_RESULTS_PATH
-    results = load_and_format_results(filepath)
+        segments = DEFAULT_SEGMENTS
+    if args.totalheader:
+        total_key = args.totalheader
+    else:
+        total_key = TOTAL_KEY
+    results = load_and_format_results(filepath, segments, total_key)
